b82d549c43dc6b1ad68dc0112715709ed41b9a0b
HP <HP@DESKTOP-QD0T1NV>
2020-08-21 14:56:29 +0530

    First Commit for performing CI using jenkins and sonarqube

---
+++ CICD_Lab2_B1/src/main/java/CICD_Lab2_B1/Continuous_integration.java
@@ -0,0 +1,17 @@
+package CICD_Lab2_B1;
+
+public class Continuous_integration {
+	private int n = 10;
+	 public void f1(int n1)
+	 {
+		 	n1=10;
+	 }
+	public static void main(String[] args) {
+	
+		System.out.println("Hello Sonar");
+		System.out.println("Performing CI");
+		
+		
+	}
+
+}
